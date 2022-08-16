c295e6469df6cd04e08de82c35dd8b87eda9445b
hikari <kanbarusuruga0127@gmail.com>
2022-08-16 11:25:24 +0900

    add: event handling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
+const app = Vue.createApp ({
+    data: ()=> ({
 
+    }),
+    methods: {
+        addItem: function (event) {
+            console.log("Clicked!")
+        }
+    }
+})
+app.mount('#app')
 
 
 
