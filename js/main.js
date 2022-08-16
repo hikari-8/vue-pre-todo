9b90a6dc1f297c887d533153f7632fc3c921b574
hikari <kanbarusuruga0127@gmail.com>
2022-08-16 11:38:36 +0900

    add: clear task and show lists

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const app = Vue.createApp ({
     data: ()=> ({
-
+        newItem: "",
+        todos: []
     }),
     methods: {
         addItem: function (event) {
-            console.log("Clicked!")
+            // console.log("Clicked!")
+            if (this.newItem === '') return
+            let todo = {
+                item: this.newItem
+            }
+            this.todos.push(todo)
+            this.newItem = ""
         }
     }
 })
